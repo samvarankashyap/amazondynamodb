5ab4c7423d715dca5dadc708a19003fccb179cf9
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2017-03-07 20:25:07 -0500

    Updated var names and the way to handle them

--- samplescripts/sample_dynamo.py
+++ samplescripts/sample_dynamo.py
@@ -3,30 +3,34 @@ from boto.dynamodb.schema import Schema
 from  boto.dynamodb.condition import *
 conn = boto.dynamodb.connect_to_region('us-west-2')
 
-def get_data(query):
-    table = conn.get_table('consumer_table')
-    #items = table.scan(scan_filter={'c_id': EQ('1427578')})
+
+def get_data(table, query):
+    table = conn.get_table(table)
+    # example query
+    # items = table.scan(scan_filter={'c_id': EQ('1427578')})
     items = table.scan(scan_filter=query)
-    #print items.response["Items"]
     return items.response["Items"]
+
 
 def prepare_query(posted_data):
     data = {}
-    for x in posted_data:
-        data[x]= CONTAINS(posted_data[x])
+    for record in posted_data:
+        data[record]= CONTAINS(posted_data[record])
     return data
+
 
 def filter_data(posted_data):
     data = {}
-    for x in posted_data:
-        if posted_data[x]!='':
-            data[x] = posted_data[x]
+    for record in posted_data:
+        if posted_data[record]!='':
+            data[record] = posted_data[record]
     return data
+
 
 posted_data = {'c_id': '14250', 'zip':''}
 posted_data = filter_data(posted_data)
 print posted_data
 posted_data = prepare_query(posted_data)
 print posted_data 
-posted_data = get_data(posted_data)
+posted_data = get_data('consumer_data',posted_data)
 print posted_data
